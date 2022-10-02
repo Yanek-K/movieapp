38ba63f483bbea9f1023f881f0030c0508cc68e0
Yanek-K <yanek.dev@gmail.com>
2022-10-02 14:00:02 -0400

    Start Fetching Data

--- pages/index.vue
+++ pages/index.vue
@@ -5,10 +5,32 @@
 </template>
 
 <script>
+import axios from 'axios'
 import HeroSection from '../components/HeroSection.vue'
 
 export default {
   name: 'IndexPage',
   components: { HeroSection },
+  env: {
+    api_key: process.env.API_KEY,
+  },
+  data() {
+    return {
+      movies: [],
+    }
+  },
+  async fetch() {
+    await this.getMovies()
+  },
+  methods: {
+    async getMovies() {
+      const data = axios.get(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.API_KEY}&language=en-US&page=1`
+      )
+      const result = await data
+
+      return result
+    },
+  },
 }
 </script>
