0a2b17531056460c116e3c1035742acbbbcaea79
Yanek-K <yanek.dev@gmail.com>
2022-10-03 12:36:55 -0400

    Build movie view

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="home">
     <HeroSection />
+
+    <div class="container movies">
+      <div id="movie-grid" class="movie-grid">
+        <div class="movie" v-for="(movie, index) in movies" :key="index">
+          <div class="movie-img">
+            <img
+              :src="`https://image.tmdb.org/t/p/${movie.poster_path}`"
+              alt=""
+            />
+            <p class="review">{{ movie.vote_average }}</p>
+            <p class="overview">{{ movie.overview }}</p>
+          </div>
+          <div class="info">
+            <p class="title">
+              {{ movie.title.slice(0, 25)
+              }}<span v-if="movie.title.length > 25">...</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -28,8 +49,8 @@ export default {
         `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.API_KEY}&language=en-US&page=1`
       )
       const result = await data
-
-      return result
+      this.movies = result.data.results
+      console.log(this.movies)
     },
   },
 }
